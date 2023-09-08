9687a01f4f11240500eb379dfc6e9bdb1ec21dd0
annotshy04 <vutruonganhyhy@gmail.com>
2023-09-08 08:21:37 +0700

    update

--- C/lc3.c
+++ C/lc3.c
@@ -2,6 +2,15 @@
 
 #include<stdio.h>
 #include<stdint.h>
+#include<signal.h>
+/* unix only */
+#include<stdlib.h>
+#include<unistd.h>
+#include<fcntl.h>
+#include<sys/time.h>
+#include<sys/types.h>
+#include<sys/termios.h>
+#include<sys/mman.h>
 
 /*
     The LC-3 has (1<<16)=65536 memory locations each of which stores a 16-bit value
@@ -29,7 +38,6 @@ enum
     R_R6,
     R_R7,
     R_PC, /* program counter */
-    R_R1,
     R_COND, /* condition flags */
     R_COUNT
 };
@@ -78,6 +86,57 @@ enum {
     OP_TRAP /* execute trap */
 };
 
+// TRAP CODEs
+enum {
+    TRAP_GETC = 0x20,  /* get character from keyboard, not echo onto the terminal */
+    TRAP_OUT = 0x21,   /* output a character */
+    TRAP_PUTS = 0x22,  /* output a word string */
+    TRAP_IN = 0x23,    /* get character from keyboard, echo onto the terminal */
+    TRAP_PUTSP = 0x24, /* output a byte string */
+    TRAP_HALT = 0x25   /* halt the program */
+};
+
+// MEMORY MAPPED REGISTERS
+enum  {
+    MR_KBSR = 0xFE00, /* keyboard status */
+    MR_KBDR = 0xFE02 /* keyboard data */
+};
+
+/* Input Buffering (?? wtf) */
+struct termios original_tio;
+
+void disable_input_buffering()
+{
+    tcgetattr(STDIN_FILENO, &original_tio);
+    struct termios new_tio = original_tio;
+    new_tio.c_lflag &= ~ICANON & ~ECHO;
+    tcsetattr(STDIN_FILENO, TCSANOW, &new_tio);
+}
+
+void restore_input_buffering()
+{
+    tcsetattr(STDIN_FILENO, TCSANOW, &original_tio);
+}
+
+uint16_t check_key()
+{
+    fd_set readfds;
+    FD_ZERO(&readfds);
+    FD_SET(STDIN_FILENO, &readfds);
+
+    struct timeval timeout;
+    timeout.tv_sec = 0;
+    timeout.tv_usec = 0;
+    return select(1, &readfds, NULL, NULL, &timeout) != 0;
+}
+
+void handle_interrupt(int signal)
+{
+    restore_input_buffering();
+    printf("\n");
+    exit(-2);
+}
+
 void update_flags(uint16_t r) {
     if (reg[r] == 0) {
         reg[R_COND] = FL_ZRO;
@@ -85,8 +144,7 @@ void update_flags(uint16_t r) {
         reg[R_COND] = FL_NEG;
     } else {
         reg[R_COND] = FL_POS;
-    }
-    
+    }    
 }
 
 uint16_t sign_extend(uint16_t x, int bit_count) {
@@ -101,11 +159,58 @@ uint16_t sign_extend(uint16_t x, int bit_count) {
    return x;
 }
 
+/* little-end -> big-end and vice versa */
+uint16_t swap16(uint16_t x) {
+    return (x << 8) | (x >> 8);
+}
+
+void read_image_file(FILE* file) {
+    /* the origin tells up where in memory to place the image */
+    uint16_t origin;
+    fread(&origin, sizeof(origin), 1, file);
+    origin = swap16(origin);
+    /* we know the maximum file size so we only need one fread */
+    uint16_t max_read = MEMORY_MAX - origin;
+    uint16_t* p = memory + origin;
+    size_t len = fread(p, sizeof(uint16_t), max_read, file);
+    /* swap to little-end */
+    while (len > 0)  {
+        len --;
+        *p = swap16(*p);
+        ++ p;
+    }
+}
+
+int read_image(const char* image_path) {
+    FILE* file = fopen(image_path, "rb");
+    if (!file) return 0;
+    read_image_file(file);
+    fclose(file);
+    return 1;
+}
+
+/* Memory Access */
+void mem_write(uint16_t address, uint16_t data) {
+    memory[address] = data;
+}
+
+uint16_t mem_read(uint16_t address) {
+    if (address == MR_KBSR) {
+        if (check_key()) {
+            memory[MR_KBSR] = (1 << 15);
+            memory[MR_KBDR] = getchar();
+        } else {
+            memory[MR_KBSR] = 0;
+        }
+    }
+    return memory[address];
+}
+
 int main(int argc, const char *argv[]) {
-    // LOAD STATEMENT
+    // LOAD ARGUMENT
     if (argc < 2) {
         /* show usage */
-        printf("lc3 [image-file1] ...\n");
+        printf("[Usage]: lc3-vm [image-file1] ...\n");
         exit(2);
     }
 
@@ -118,6 +223,8 @@ int main(int argc, const char *argv[]) {
 
 
     // SETUP
+    signal(SIGINT, handle_interrupt);
+    disable_input_buffering();
 
     // MAIN LOOP
     /* since exactly one conditon flag should be given at any given time, set the Z flag*/
@@ -277,13 +384,78 @@ int main(int argc, const char *argv[]) {
                 }
                 break;
             case OP_TRAP:
+                {
+                    reg[R_R7] = reg[R_PC];
+                    switch (instr & 0xFF) {
+                        case TRAP_GETC:
+                            {
+                                reg[R_R0] = (uint16_t) getchar();
+                                update_flags(R_R0);
+                            }
+                            break;
+                        case TRAP_OUT:
+                            {
+                                putc((char) reg[R_R0], stdout);
+                                fflush(stdout);   
+                            }
+                            break;
+                        case TRAP_PUTS:
+                            {
+                                uint16_t* c = memory + reg[R_R0];
+                                while (*c) {
+                                    putc((char)*c, stdout);
+                                    ++ c;
+                                }
+                                fflush(stdout);
+                            }
+                            break;
+                        case TRAP_IN:
+                            {
+                                printf("Enter a character: ");
+                                char c = getchar();
+                                putc(c, stdout);
+                                fflush(stdout);
+                                reg[R_R0] = (uint16_t) c;
+                                update_flags(R_R0);
+                            }
+                            break;
+                        case TRAP_PUTSP:
+                            {
+                                /* one char per byte (two bytes per word) -> need swap back to big-end*/
+                                uint16_t *c = memory + reg[R_R0];
+                                while (*c) {
+                                    char char1 = *c & 0xFF; // 1111 1111, take last 8 bits = 1 byte
+                                    putc(char1, stdout);
+                                    char char2 = *c >> 8; // first 8 bits
+                                    if (char2) putc(char2, stdout);
+                                    ++ c;
+                                }
+                                fflush(stdout);
+                                
+                            }
+                            break;
+                        case TRAP_HALT:
+                            {
+                                puts("HALT");
+                                fflush(stdout);
+                                running = 0;
+                            }
+                            break;                        
+                        default:
+                            break;
+                    }
+                }
                 break;
             case OP_RES:
             case OP_RTI:
             default:
+                // BAD OPCODE
+                abort();
                 break;
         }
     }
     
+    // SHUTDOWN
+    restore_input_buffering();
 
 }
